da601f96eeeb914ed01edd0041224a4613e8abe0
shanchaicn <shanchaicn@gmail.com>
2024-03-02 21:34:35 -0500

    test

---
+++ aaqr_ros2_py/save_poses_node.py
@@ -0,0 +1,119 @@
+from docutils import SettingsSpec
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseWithCovarianceStamped
+import yaml
+
+import sys, select
+
+import tty, termios
+import os
+
+home_dir = os.path.expanduser('~')
+
+filepath = os.path.join(home_dir, 'current_pose.yaml')
+
+class PoseSaver(Node):
+    def __init__(self):
+
+        super().__init__('pose_saver')
+        self.subscription = self.create_subscription(
+            PoseWithCovarianceStamped,
+            '/pose',
+            self.pose_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.get_logger().info("Press 's' to save the current pose to YAML file.")
+        
+
+    def pose_callback(self, msg):
+        self.current_pose = msg.pose.pose
+        self.get_logger().info(f"Received posex: {self.current_pose.position.x}")
+        self.get_logger().info(f"Received posey: {self.current_pose.position.y}")
+
+
+    def save_pose_to_yaml(self, file_path):
+        pose_dict = {
+            'position': {
+                'x': self.current_pose.position.x,
+                'y': self.current_pose.position.y,
+                'z': self.current_pose.position.z
+            },
+            'orientation': {
+                'x': self.current_pose.orientation.x,
+                'y': self.current_pose.orientation.y,
+                'z': self.current_pose.orientation.z,
+                'w': self.current_pose.orientation.w
+            }
+        }
+        with open(file_path, 'w') as yaml_file:
+            yaml.dump(pose_dict, yaml_file)
+            self.get_logger().info(f"Pose saved to {file_path}")
+    
+    def save_points(self, pose_id, pose_yaml, file_path):
+        pose_dict = {
+            'position': {
+                'x': self.current_pose.position.x,
+                'y': self.current_pose.position.y,
+                'z': self.current_pose.position.z
+            },
+            'orientation': {
+                'x': self.current_pose.orientation.x,
+                'y': self.current_pose.orientation.y,
+                'z': self.current_pose.orientation.z,
+                'w': self.current_pose.orientation.w
+            }
+        }
+        pose_yaml[pose_id] = pose_dict
+        with open(file_path, 'w') as yaml_file:
+            yaml.dump(pose_yaml, yaml_file)
+            self.get_logger().info("Saved points to {}".format(filepath))
+
+    def delete_last_pose(self, points_yaml, filepath):
+        if points_yaml:
+            last_pose_id = max(points_yaml.keys())
+            del points_yaml[last_pose_id]
+            with open(filepath, "w") as yamlfile:
+                yaml.dump(points_yaml, yamlfile)
+            self.get_logger().info("Deleted the last saved pose (ID: {})".format(last_pose_id))
+        else:
+            self.get_logger().info("No poses to delete.")
+
+    def run(self):
+        if os.path.exists(filepath):
+            self.get_logger().info("The YAML file already exists. Quitting the node.")
+            pass
+        else:
+            points_yaml = {}
+            pose_id = 1
+            try:
+                while rclpy.ok():
+                    rclpy.spin_once(self)
+                    key = input("Press 's' to save pose, 'd' to delete last pose, 'q' to quit: ")
+                    if key == 's':
+                        self.save_points(pose_id,points_yaml,filepath)
+                        print("==== save point {} ====".format(pose_id))
+                        pose_id = pose_id + 1
+                    elif key == 'd':
+                        self.delete_last_pose(points_yaml, filepath)
+                        print("=== delete last pose ===")
+                        pose_id = pose_id - 1
+                    elif key == 'q':
+                        break
+            except KeyboardInterrupt:
+                pass
+            finally:
+                self.get_logger().info("Shutting down...")
+
+
+def main():
+    rclpy.init()
+    pose_saver = PoseSaver()
+    pose_saver.run()
+    print('node end')
+    rclpy.shutdown()
+    print('rclpy end')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'aaqr_ros2_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shan',
+    maintainer_email='shan@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'save_poses_node = aaqr_ros2_py.save_poses_node:main'
+        ],
+    },
+)
