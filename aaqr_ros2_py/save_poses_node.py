fc34eb07053f96b3bcd9c4ee365b737c4d112afb
shanchaicn <140185688+shanchaicn@users.noreply.github.com>
2024-05-17 14:49:58 -0400

    Update save_poses_node.py
    add threading

--- aaqr_ros2_py/save_poses_node.py
+++ aaqr_ros2_py/save_poses_node.py
@@ -1,12 +1,9 @@
-
+import os
+import threading
+import yaml
 import rclpy
 from rclpy.node import Node
-
-import yaml
-import os
 from tf2_ros import TransformException, Buffer, TransformListener
-
-
 
 home_dir = os.path.expanduser('~')
 aaqr_dir = os.path.join(home_dir, 'aaqr')
@@ -15,28 +12,28 @@ os.makedirs(pose_dir, exist_ok=True)
 
 class PoseSaver(Node):
     def __init__(self):
-        super().__init__('pose_saver')   
+        super().__init__('pose_saver')
         self.map_name = self.declare_parameter('map_name', 'map').get_parameter_value().string_value
-        
         self.filepath = os.path.join(pose_dir, f'{self.map_name}.yaml')
-        
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
-        while rclpy.ok():
-            try:
-                self.t=self.tf_buffer.lookup_transform('map', 'base_link', rclpy.time.Time())
-                self.get_logger().info("Transform 'map' to 'base_link' is available.")
-                break
-            except TransformException as ex:
-                rclpy.spin_once(self)
+        
+        self.spin_thread = threading.Thread(target=self.spin)
+        self.spin_thread.start()
+        
         self.get_logger().info("Save_poses_node to save the current pose with keyboard.")
-        self.get_logger().info("%s" %self.filepath)
+        self.get_logger().info(f"{self.filepath}")
+
+    def spin(self):
+        while rclpy.ok():
+            rclpy.spin_once(self)
 
     def save_points(self, pose_id, pose_yaml, file_path):
         while rclpy.ok():
             try:
-                self.t=self.tf_buffer.lookup_transform('map', 'base_link', rclpy.time.Time())
+                self.t = self.tf_buffer.lookup_transform('world', 'turtle1', rclpy.time.Time())
                 self.get_logger().info("Transform 'map' to 'base_link' is available.")
+                
                 pose_dict = {
                     'position': {
                         'x': self.t.transform.translation.x,
@@ -50,22 +47,24 @@ class PoseSaver(Node):
                         'w': self.t.transform.rotation.w
                     }
                 }
+                
                 pose_yaml[pose_id] = pose_dict
-                self.get_logger().info("x= %0.2f,y=%0.2f."% (self.t.transform.translation.x, self.t.transform.translation.y))
+                self.get_logger().info(f"x= {self.t.transform.translation.x:.2f}, y={self.t.transform.translation.y:.2f}")
+                
                 with open(file_path, 'w') as yaml_file:
                     yaml.dump(pose_yaml, yaml_file)
                     self.get_logger().info("pose updated")
                 break
-            except TransformException as ex:
+            except TransformException:
                 pass
 
     def delete_last_pose(self, points_yaml, filepath):
         if points_yaml:
             last_pose_id = max(points_yaml.keys())
             del points_yaml[last_pose_id]
-            with open(filepath, "w") as yamlfile:
-                yaml.dump(points_yaml, yamlfile)
-            self.get_logger().info("Deleted the last saved pose (ID: {})".format(last_pose_id))
+            with open(filepath, 'w') as yaml_file:
+                yaml.dump(points_yaml, yaml_file)
+            self.get_logger().info(f"Deleted the last saved pose (ID: {last_pose_id})")
         else:
             self.get_logger().info("No poses to delete.")
 
@@ -81,7 +80,7 @@ class PoseSaver(Node):
                     key = input("Press 's' to save pose, 'd' to delete last pose, 'q' to quit: ")
                     if key == 's':
                         self.save_points(pose_id, points_yaml, self.filepath)
-                        print("==== save point {} ====".format(pose_id))
+                        print(f"==== save point {pose_id} ====")
                         pose_id += 1
                     elif key == 'd':
                         self.delete_last_pose(points_yaml, self.filepath)
@@ -101,8 +100,8 @@ def main():
         node.run()
     except KeyboardInterrupt:
         pass
-
     rclpy.shutdown()
+    node.spin_thread.join()
 
 if __name__ == '__main__':
     main()
